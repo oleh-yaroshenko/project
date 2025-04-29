b1dd7decdfa2f4ccf1490805bdb992a695c4bb6d
back <asagacka@gmail.com>
2025-04-29 19:29:10 +0300

    add header

--- script.js
+++ script.js
@@ -7,6 +7,14 @@
 
 
 // нижче буде код секції header (щоб було бургер меню) (секція Назара)
+function toggleMenu() {
+    document.getElementById('navMenu').classList.toggle('open');
+  }
+  
+  function buyNow() {
+    alert("Redirecting to purchase...");
+  }
+  
 
 
 
