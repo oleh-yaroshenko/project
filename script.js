834fae907de1d3fb053169da3646135fdb406a2e
misha <fff650625@gmail.com>
2025-05-08 22:01:14 +0300

    [робив трейлер начаы faq+footer]

--- script.js
+++ script.js
@@ -11,9 +11,27 @@
 
 
 // нижче буде код секції trailer (щоб відкривалася модалка при натисканні на кнопку з відео ютуб) (секція Міші)
+const playButton = document.querySelector('.trailer-play-button')
+const videoOverlay = document.getElementById('trailer-video-overlay')
 
+let videoOpen = false;
 
-
+playButton.addEventListener('click', () => {
+if (!videoOpen){
+    videoOverlay.innerHTML = `
+  <iframe src="https://www.youtube.com/embed/K0u_kAWLJOA?autoplay=1"
+                allow="autoplay; fullscreen"
+                allowfullscreen>
+        </iframe>`;
+        videoOverlay.style.display = 'flex';
+        videoOpen = true;
+}
+else {
+videoOverlay.innerHTML = '';
+videoOverlay.style.display = 'none';
+videoOpen - false;
+}
+});
 // нижче буде код секції series (щоб працювали switch оператори і мінявся текст 2 секцій відповідно) (секція Марка)
 
 
