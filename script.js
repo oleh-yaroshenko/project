95e49ca862c58a23ad9205b28cc9e4f3f47d9165
Oleh Yaroshenko <oleg21062010@gmail.com>
2025-05-12 17:33:18 +0300

    Add misha code manually

--- script.js
+++ script.js
@@ -106,7 +106,27 @@ headerBurger.addEventListener("click", () => {
 
 // нижче буде код секції trailer (щоб відкривалася модалка при натисканні на кнопку з відео ютуб) (секція Міші)
 
+const trailerPlayButton = document.querySelector('.trailer-play-button')
+const trailerVideoOverlay = document.getElementById('trailer-video-overlay')
 
+let videoOpen = false;
+
+trailerPlayButton.addEventListener('click', () => {
+    if (!videoOpen) {
+        trailerVideoOverlay.innerHTML = `
+  <iframe src="https://www.youtube.com/embed/K0u_kAWLJOA?autoplay=1"
+                allow="autoplay; fullscreen"
+                allowfullscreen>
+        </iframe>`;
+        trailerVideoOverlay.style.display = 'flex';
+        videoOpen = true;
+    }
+    else {
+        trailerVideoOverlay.innerHTML = '';
+        trailerVideoOverlay.style.display = 'none';
+        videoOpen - false;
+    }
+});
 
 // нижче буде код секції series (щоб працювали switch оператори і мінявся текст 2 секцій відповідно) (секція Марка)
 
